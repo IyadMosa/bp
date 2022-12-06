9901a36440fcbaafc742a51849a6766b2c974e1b
IyadA <iyad_mosa@hotmail.com>
2022-12-06 11:13:52 +0200

    remove uneeded code / logo /

--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {Button, TextField} from "@iyadmosa/react-library/dist/esm";
 import {useState} from "react";
@@ -9,29 +8,15 @@ function App() {
     const [name, setName] = useState("");
 
     return (
-    <div className="App">
-      <TextField title={"rrrrff"}
-                 value={name}
-                 onChange={(value) => setName(value)}
-                 disabled={false}></TextField>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button label={"test222"}></Button>
+        <div>
+            <TextField title={"rrrrff"}
+                       value={name}
+                       onChange={(value) => setName(value)}
+                       disabled={false}></TextField>
+            <Button label={"test222"}></Button>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
