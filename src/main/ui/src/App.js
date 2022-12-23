c5e6009b061578d0e16714926fa3c25bde485017
IyadA <iyad_mosa@hotmail.com>
2022-12-23 21:57:35 +0200

    use MainScreen in app

--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -2,12 +2,33 @@ import "./App.css";
 import { Main } from "./components/main/Main";
 import { Provider } from "react-redux";
 import store from "./store";
+import { MainScreen, EmptyScreen } from "@iyadmosa/react-library";
+import { Deposits } from "./components/deposit/Deposits";
+import { Withdraws } from "./components/withdraw/Withdraws";
+import React from "react";
 function App() {
+  const brand = { name: "NavbarScroller", to: "/main" };
+  const links = [
+    {
+      name: "Main",
+      to: "/main",
+      component: <EmptyScreen title={"Main Page"} page={<Main />} />,
+    },
+    {
+      name: "Deposits",
+      to: "/deposits",
+      component: <Deposits />,
+    },
+    {
+      name: "Withdraws",
+      to: "/withdraws",
+      component: <Withdraws />,
+    },
+  ];
+
   return (
     <Provider store={store}>
-      <div>
-        <Main></Main>
-      </div>
+      <MainScreen brand={brand} links={links} />
     </Provider>
   );
 }
