8e0ae9548e7df16df9a7f7bfea1d8dd9420d6d5a
IyadA <iyada@radware.com>
2023-08-09 20:32:52 +0300

    format the code and adding .nmprc file

--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -1,49 +1,49 @@
 import "./App.css";
-import { Dashboard } from "./components/dashboard/Dashboard";
-import { Provider } from "react-redux";
+import {Dashboard} from "./components/dashboard/Dashboard";
+import {Provider} from "react-redux";
 import store from "./store";
-import { MainScreen } from "@iyadmosa/react-library";
-import { Deposits } from "./components/deposit/Deposits";
-import { Withdraws } from "./components/withdraw/Withdraws";
+import {MainScreen} from "@iyadmosa/react-library";
+import {Deposits} from "./components/deposit/Deposits";
+import {Withdraws} from "./components/withdraw/Withdraws";
 import React from "react";
-import { Reasons } from "./components/reason/Reasons";
-import { Persons } from "./components/person/Persons";
+import {Reasons} from "./components/reason/Reasons";
+import {Persons} from "./components/person/Persons";
 
 function App() {
-  const brand = { name: "NavbarScroller", to: "/dashboard" };
-  const links = [
-    {
-      name: "Dashboard",
-      to: "/dashboard",
-      component: <Dashboard />,
-    },
-    {
-      name: "Deposits",
-      to: "/deposits",
-      component: <Deposits />,
-    },
-    {
-      name: "Withdraws",
-      to: "/withdraws",
-      component: <Withdraws />,
-    },
-    {
-      name: "Persons",
-      to: "/persons",
-      component: <Persons />,
-    },
-    {
-      name: "Reasons",
-      to: "/reasons",
-      component: <Reasons />,
-    },
-  ];
+    const brand = {name: "NavbarScroller", to: "/dashboard"};
+    const links = [
+        {
+            name: "Dashboard",
+            to: "/dashboard",
+            component: <Dashboard/>,
+        },
+        {
+            name: "Deposits",
+            to: "/deposits",
+            component: <Deposits/>,
+        },
+        {
+            name: "Withdraws",
+            to: "/withdraws",
+            component: <Withdraws/>,
+        },
+        {
+            name: "Persons",
+            to: "/persons",
+            component: <Persons/>,
+        },
+        {
+            name: "Reasons",
+            to: "/reasons",
+            component: <Reasons/>,
+        }
+    ];
 
-  return (
-    <Provider store={store}>
-      <MainScreen brand={brand} links={links} />
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <MainScreen brand={brand} links={links}/>
+        </Provider>
+    );
 }
 
 export default App;
